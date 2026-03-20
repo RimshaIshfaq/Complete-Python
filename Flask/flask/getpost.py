80082361adc3d9f3f7389f35c1ec82671742d438
Rimsha Ishfaq <rimshaishfaq86@gmail.com>
2026-03-20 09:34:37 +0500

    Working with HTTP verbs GET and POST

---
+++ Flask/flask/getpost.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request 
+#render_template will be used to redirect it to html page
+
+'''
+It creates an instance of the Flask class,
+which will be your WSGI (Web Server Gateway Interface) Application
+'''
+#WSGI Application
+
+app = Flask(__name__)
+
+@app.route('/')
+def Welcome():
+    return '<html><h1> Welcome to my the Flask </h1></html>'
+
+@app.route('/index',methods=['GET']) #if you do not use any method, default method is GET
+def index():
+    return render_template('index.html') # it will try to look it in folder templates
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+if __name__ =="__main__":
+    app.run(debug=True) 
+    # debug = True will keep on adding chnages while running. 
+    # It means we do not need to restart server again and again
+
+
+    
